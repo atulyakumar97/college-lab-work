00a6ceac3d718747b9e7b07cdc07c9844d5ba779
atulyakumar97 <atulyakumar97@gmail.com>
2018-10-26 18:17:51 +0530

    DCOM

--- DCOM/07. (n,k) Linear Block Code/dcom7.m
+++ DCOM/07. (n,k) Linear Block Code/dcom7.m
@@ -1,56 +1,49 @@
-clc;
-clear all;
+clc
+close all
+clear all
+ 
+G=double(input('Enter G : '))
+[m,n]=size(G);
+M=input('Enter M : ')
+ 
+I=G(:,1:4)
+P=G(:,5:7)
+ 
+disp('Transmitted Codeword: ')
+C=[M,mod(M*P,2)]
+ 
+%GENERATING ERROR
+r=C;
+z=randi(7);
+r(z)=mod(r(z)+1,2);
 
-n=input('Enter rows : ');
-k=input('Enter cols : ');
+disp('Received Codeword:')
+disp(r)
+%====================
+ 
 
-str = inputdlg('Enter values column wise');
-numbers = str2num(str{1});
-G = reshape(numbers,n,k);
+H=gen2par(G)
+Ht=H'
+S=mod(r*Ht,2)
+[l,k]=size(S);
+ 
 
-
-
-I = G(:,1:4);
-P = G(:,5:7);
-
-M = [1 0 1 1];
-C = M*P;
-for i = 1:3
-    C(i) = mod(C(i),2);
-end
-A = cat(2,M,C);
-
-r = A(:,:);
-z = randi(4)
-r(z) = r(z)+1;
-r(z) = mod(r(z),2);
-disp('error message: ');
-disp(r);
-H = gen2par(G);
-S = r*H';
-for i = 1:3
-    S(i) = mod(S(i),2);
-end
-
-temp = H';
-for i = 1:n
-    s = 0;
-    for j = 1:3
-        if S(j) == temp(i,j)
-            s= s+1;
-        end
-        if s == 3;
-            p = i;
-            r(i) = r(i)+1;
-            break;
+for i=1:m
+    count=0;
+    for j=1:k
+        if S(j)==Ht(i,j)
+            count=count+1;
+            if count==k
+                break 
+            end
+            break
         end
     end
 end
+ 
+disp('Error in bit ')
+ disp(i);
+  r(i)=mod(r(i)+1,2);
 
-for i = 1:3
-    r(i) = mod(r(i),2);
-end
-disp('error in bit number: ');
-disp(p);
-disp('corrected message: ');
-disp(r);
+disp('Corrected Codeword :')
+disp(r)
