96b09dd11598a27fb47f7cfb499a4664770f9600
atulyakumar97 <atulyakumar97@gmail.com>
2019-08-10 21:10:49 +0530

    NNFL
    Co-Authored-By: Viren Baria <virenbaria17@gmail.com>

---
+++ SEM 07/NNFL/4/README.md
@@ -0,0 +1,9 @@
+# In this experiment, we have performed Mamdani style fuzzy interface for 2 input and 2 output.
+
+### The process is performed as follows --
+
+## 1. Fuzzification        
+## 2. Setting rules
+## 3. Inference
+## 4. Defuzzification
+
---
+++ SEM 07/NNFL/4/expt4_a.m
@@ -0,0 +1,40 @@
+clc
+clear all
+close all
+
+img=im2double(rgb2gray(imread('peppers.png')));
+img = imresize(img,[256 256]);
+a=max(max(img));
+b=min(min(img));
+img_norm=(img-b)/(a-b);
+gx=[-1 1];
+gy=gx';
+Ix=conv2(img_norm,gx);
+Iy=conv2(img_norm,gy);
+
+fis1=readfis('expt4');
+getfis(fis1);
+showrule(fis1);
+
+for i=1:256
+    for j=1:256
+        Ieval(i,j)=evalfis([Ix(i,j); Iy(i,j)]',fis1);
+    end
+end
+
+figure(1)
+subplot(2,2,1)
+imshow(img)
+title('Original Image')
+
+subplot(2,2,2)
+imshow(Ix)
+title('Gradient-X Image')
+
+subplot(2,2,3)
+imshow(Iy)
+title('Gradient-Y Image')
+
+subplot(2,2,4)
+imshow(Ieval)
+title('Edge-Detected Image')
