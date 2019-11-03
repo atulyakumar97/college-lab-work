6ccdf95edee44d532c6251bcf8685933d0d1c8d6
atulyakumar97 <atulyakumar97@gmail.com>
2019-11-03 20:19:00 +0530

    NNFL

--- SEM 07/NNFL/9. RBF for Non Linear Data Classification/nnfl9.m
+++ SEM 07/NNFL/9. RBF for Non Linear Data Classification/nnfl9.m
@@ -1,17 +1,28 @@
-clc
-clear all
-close all
-pi=3.14159
-x=0:0.25:4
-y=sin(pi*x)+cos(pi*x);
-net=newrb(x,y);
-view(net)
-x1=0:0.2:4
-y1=sim(net,x1)
-figure
+clc;
+clear all;
+close all;
+A = [rand(1,100);rand(1,100)];
+B = [-1.*rand(1,100);rand(1,100)];
+C = [-1.*rand(1,100);-1.*rand(1,100)];
+D = [rand(1,100);-1.*rand(1,100)];
+
 hold on
-plot(x,y,'*')
-plot(x1,y1,'o')
-legend('Training','Testing')
-xlabel('Inputs')
-ylabel('Targets')
+scatter(A(1,:), A(2,:),'x','r')
+scatter(B(1,:), B(2,:),'o','b')
+scatter(C(1,:), C(2,:),'.','g')
+scatter(D(1,:), D(2,:),'^','k')
+
+P=[A(:,1:80) B(:,1:80) C(:,1:80) D(:,1:80)];
+At = repmat([1 0 0 0], 80, 1);
+Bt = repmat([0 1 0 0], 80, 1);
+Ct = repmat([0 0 1 0], 80, 1);
+Dt = repmat([0 0 0 1], 80, 1);
+T = [At' Bt' Ct' Dt'];
+
+size(P)
+size(T)
+
+net = newrb(P,T);
+y=round(sim(net,A(:,81:100)));
+view(net);
+
